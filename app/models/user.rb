3afab2a3978c11b1e16192889e119dfc3bd9bf1c
pola91 <polamoussa@gmail.com>
2021-03-17 01:50:54 +0200

    create payment method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   include Pay::Billable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
   extend Devise::Models
 
   devise :database_authenticatable, :registerable,
